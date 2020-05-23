9ee82534f539104e72b016904ed605ffb1e72ba0
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-05-23 21:21:01 +0200

    add script for manual tasking

--- scripts/q2.py
+++ scripts/q2.py
@@ -1,13 +1,14 @@
 import sys
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-from lib.task import ManualTask, TaskMaster
+from lib.Tasks import ManualTask, TaskMaster
 
 DURATION = 5
 
 ts = []
 
 ts.append(ManualTask("task1", [(0, DURATION)]))
+ts.append(ManualTask("task2", [(DURATION, 0)]))
 ts.append(ManualTask("task3", [(DURATION, DURATION)]))
 ts.append(ManualTask("task2", [(DURATION*2, DURATION)]))
 
